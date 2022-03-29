1ff0854479f8957ea7cb9dd3d3791425f873bb8c
bacf5 <brunoagustincarusofassa@gmail.com>
2022-03-29 17:57:10 +0200

    dice function

---
+++ script.js
@@ -0,0 +1,22 @@
+function diceRoll() {
+  const dice = [...document.querySelectorAll('.die-list')];
+  dice.forEach((die) => {
+    toggleClasses(die);
+    die.dataset.roll = getRandomNumber(1, 6);
+    // checking
+    console.log(die.dataset.roll);
+  });
+}
+
+function toggleClasses(die) {
+  die.classList.toggle('odd-roll');
+  die.classList.toggle('even-roll');
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById('roll-button').addEventListener('click', diceRoll);
